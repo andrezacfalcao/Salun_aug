72dd3792728dad0e78f211d9d06965f02fd16698
filiperobotic <filipe.rolim@gmail.com>
2025-01-08 16:59:03 -0300

    dbg

--- Classification/dataset.py
+++ Classification/dataset.py
@@ -771,6 +771,7 @@ def medmnist_dataloaders(
         raise ValueError(
             "Only one of `class_to_replace` and `indexes_to_replace` can be specified"
         )
+    import pdb; pdb.set_trace()
     if class_to_replace is not None:
         replace_class(
             train_set,
@@ -782,7 +783,7 @@ def medmnist_dataloaders(
         #if num_indexes_to_replace is None or num_indexes_to_replace == 4500:
         if num_indexes_to_replace is None :
             #test_set.data = test_set.data[test_set.targets != class_to_replace]
-            import pdb; pdb.set_trace()
+            
             test_set.imgs = test_set.imgs[test_set.labels != class_to_replace]
             test_set.labels = test_set.labels[test_set.labels != class_to_replace]
     if indexes_to_replace is not None:
